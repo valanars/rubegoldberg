068b00b7a5fb8bb8d3bedfbdc35b6ac8d118253a
Valentine Morin <zm724@nyu.edu>
2018-02-11 17:05:55 -0500

    ok i messed up the first time ACTUALLY scripts now

---
+++ Assets/startScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startScript : MonoBehaviour {
+	
+	public GameObject Orb;
+	public Rigidbody orbBod;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		orbBod = Orb.GetComponent<Rigidbody>();
+		orbBod.useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			orbBod.useGravity = true;
+		}
+	}
+}
---
+++ Assets/startSwing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startSwing : MonoBehaviour
+{
+
+	public GameObject Hammer;
+	public Rigidbody ham;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		ham = Hammer.GetComponent<Rigidbody>();
+		ham.useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+	
+	private void OnTriggerEnter(Collider other)
+	{
+		//start swinging when domino hits button
+		if (other.gameObject.tag == "Domino")
+		{
+			ham.useGravity = true;
+		}
+
+	}
+}
