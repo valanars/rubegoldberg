0fb443069f726a4c5f0fe6df4975d019ceca5136
Valentine Morin <zm724@nyu.edu>
2018-02-11 20:10:46 -0500

    camera switch but actually

---
+++ Assets/Scripts/ballRoll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballRoll : MonoBehaviour
+{
+
+	public GameObject FinalBall;
+	public Rigidbody ball;
+
+	// Use this for initialization
+	void Start()
+	{
+		ball = FinalBall.GetComponent<Rigidbody>();
+		ball.useGravity = false;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		//start swinging when domino hits button
+		if (other.gameObject.tag == "Seesaw")
+		{
+			ball.useGravity = true;
+		}
+	}
+}
---
+++ Assets/camScripts.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camScripts : MonoBehaviour
+{
+
+	public Camera firstCam;
+
+	public Camera secondCam;
+
+	public float minX;
+	public float maxX;
+
+	public float moveSpeed;
+	public bool move;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		move = false;
+		firstCam.enabled = true;
+		
+		secondCam.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// move script activates when space key is pressed
+		if (Input.GetKey(KeyCode.Space))
+		{
+			move = true;
+		}
+		if (move == true)
+		{
+			Vector3 currentPos = transform.position;
+
+			currentPos.x += moveSpeed * Time.deltaTime;
+			if (currentPos.x > maxX)
+			{
+				currentPos.x = maxX;
+				moveSpeed = -moveSpeed;
+			} else if (currentPos.x <= minX)
+			{
+				firstCam.enabled = false; //switch cameras!
+				secondCam.enabled = true;
+			}
+
+			transform.position = currentPos;
+		}
+	}
+}
