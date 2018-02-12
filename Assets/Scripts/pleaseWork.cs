a46d0b69993a33d597672c09f202dbe87de67670
Valentine Morin <zm724@nyu.edu>
2018-02-11 23:51:44 -0500

    final build, fixed bug where ball auto fell

---
+++ Assets/Scripts/pleaseWork.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pleaseWork : MonoBehaviour {
+
+	public Text text;
+
+	public Rigidbody r;
+
+	// Use this for initialization
+	void Start()
+	{
+		r = GetComponent<Rigidbody>();
+		r.isKinematic = false;
+
+		text.enabled = false;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		//start swinging when domino hits button
+		if (other.gameObject.tag == "End")
+		{
+			r.useGravity = false;
+			r.isKinematic = true;
+
+			text.enabled = true;
+			text.text = "the end!";
+		}
+	}
+}
